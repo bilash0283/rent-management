dd0428436ee50c4880b624e0ebba7620add595a9
bilash0283 <bilash0283@gmail.com>
2026-01-24 14:53:39 +0600

    full paroject stactuser modify

--- app/controllers/DashboardController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/../helpers/auth.php";
-auth_check();
-
-class DashboardController {
-    public function index(){
-        require __DIR__ . "/../../resources/views/dashboard/index.php";
-    }
-}
