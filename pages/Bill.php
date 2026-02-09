105d166f0104ff9754ed45e8c2819a650b072bd9
bilash0283 <bilash0283@gmail.com>
2026-02-09 15:51:23 +0600

    new commit

--- pages/Bill.php
+++ pages/Bill.php
@@ -1,6 +1,6 @@
-<?php 
-    $query  = "SELECT * FROM unit wHERE status = 'Rented' ORDER BY id DESC";
-    $result = mysqli_query($db, $query);
+<?php
+$query = "SELECT * FROM unit wHERE status = 'Rented' ORDER BY id DESC";
+$result = mysqli_query($db, $query);
 
 ?>
 
@@ -36,46 +36,78 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <?php 
-                                while($row = mysqli_fetch_assoc($result)){
-                                    $unit_id = $row['id'];
-                                    $advance = $row['advance'];
-                                    $rent = $row['rent'];
-                                    $Gas = $row['Gas'];
-                                    $Water = $row['Water'];
-                                    $Electricity = $row['Electricity'];
-                                    $Internet = $row['Internet'];
-                                    $Maintenance = $row['Maintenance'];
-                                    $Others = $row['Others'];
-                                    $building_name = $row['building_name'];
-                                
-                            ?>
-                            <tr>
-                                <td>
-                                    <?php 
-                                        $sql_tenant = mysqli_query($db,"SELECT * FROM tenants WHERE building_id = '$building_name' AND unit_id = '$unit_id' ");
-                                        while($tent_row = mysqli_fetch_assoc($sql_tenant)){
+                            <?php
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                $unit_id = $row['id'];
+                                $advance = $row['advance'];
+                                $rent = $row['rent'];
+                                $Gas = $row['Gas'];
+                                $Water = $row['Water'];
+                                $Electricity = $row['Electricity'];
+                                $Internet = $row['Internet'];
+                                $Maintenance = $row['Maintenance'];
+                                $Others = $row['Others'];
+                                $building_name = $row['building_name'];
+
+                                ?>
+                                <tr>
+                                    <td>
+                                        <?php
+                                        $sql_tenant = mysqli_query($db, "SELECT * FROM tenants WHERE building_id = '$building_name' AND unit_id = '$unit_id' ");
+                                        while ($tent_row = mysqli_fetch_assoc($sql_tenant)) {
                                             $name = $tent_row['name'];
+                                            $tent_id = $tent_row['id'];
 
                                             echo $name;
                                         }
-                                    ?>
-                                </td>
-                                <td>
-                                    <?php echo $advance; ?>
-                                </td>
-                                <td>
-                                    Rent = ৳ <?php echo $rent; ?><br>
-                                    <?php if(!empty($Gas)){echo 'Gas = ৳ '.$Gas;} ?><br>
-                                    <?php if(!empty($Water)){echo 'Water = ৳ '.$Water;} ?><br>
-                                    <?php if(!empty($Electricity)){echo 'Gas = ৳ '.$Electricity;} ?><br>
-                                    <?php if(!empty($Internet)){echo 'Gas = ৳ '.$Internet;} ?><br>
-                                    <?php if(!empty($Others)){echo 'Gas = ৳ '.$Others;} ?><br>
-                                    Total Amount = ৳ <?php echo $rent+$Gas+$Water+$Electricity+$Internet+$Others; ?>
-                                </td>
-                            </tr>
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                        echo 'Advance = ৳ ' . $advance . '<br>';
+                                        echo 'Paid    = ৳ ' . $advance . '<br>';
+                                        echo 'Due     = ৳ 00';
+                                        ?>
+                                        <a href="javascript:void(0)" class="btn btn-sm btn-info mt-1" data-toggle="modal"
+                                            data-target="#ielts_show<?php echo $tent_id ?>">
+                                            Details
+                                        </a>
+                                    </td>                                   
+
+                                    <td>
+                                        Rent = ৳ <?php echo $rent; ?><br>
+                                        <?php if (!empty($Gas)) {
+                                            echo 'Gas = ৳ ' . $Gas;
+                                        } ?><br>
+                                        <?php if (!empty($Water)) {
+                                            echo 'Water = ৳ ' . $Water;
+                                        } ?><br>
+                                        <?php if (!empty($Electricity)) {
+                                            echo 'Gas = ৳ ' . $Electricity;
+                                        } ?><br>
+                                        <?php if (!empty($Internet)) {
+                                            echo 'Gas = ৳ ' . $Internet;
+                                        } ?><br>
+                                        <?php if (!empty($Others)) {
+                                            echo 'Gas = ৳ ' . $Others;
+                                        } ?><br>
+                                        Total Amount = ৳ <?php echo $rent + $Gas + $Water + $Electricity + $Internet + $Others; ?>
+                                        <a href="" class="btn btn-sm btn-info">Details</a>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-sm btn-success">Paid</button>
+                                    </td>
+                                    <td>
+                                        <a href="" class="btn btn-sm btn-light-info me-1" title="Invoice">
+                                            <i class="feather-download"></i>
+                                        </a>
+                                        <a href=">" class="btn btn-sm btn-light-primary" title="Edit">
+                                            <i class="feather-edit"></i>
+                                        </a>
+                                    </td>
+                                </tr>
                             <?php } ?>
-                        </tbody>                      
+                        </tbody>
                     </table>
                 </div>
             </div>
