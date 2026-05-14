22a80c0bf0b0611b01facfffbdbd9bb1826310a2
bilash0283 <bilash0283@gmail.com>
2026-05-14 14:41:52 +0600

    index page modify

--- index.php
+++ index.php
@@ -56,20 +56,18 @@
 <head>
     <meta charset="UTF-8">
     <title>RENT MANAGER</title>
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="shortcut icon" type="image/x-icon" href="public/assets/images/favicon.ico" />
     <!-- Bootstrap -->
     <link rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <!-- Font Awesome -->
     <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <link rel="manifest" href="manifest.json">
     <meta name="theme-color" content="#0d6efd">
-
     <style>
 
         *{
